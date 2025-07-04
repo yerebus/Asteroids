747a1615146dddc93d121e6f10b51e31fe631d35
moooseontheloose <schoolinglife@proton.me>
2025-07-04 13:05:59 -0400

    E: grouping movement/draw

--- main.py
+++ main.py
@@ -2,22 +2,38 @@ import pygame
 from constants import *
 from player import Player
 
+
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)         
-        screen.fill((0,0,0))
-        player.draw(screen)
+
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        #60 FPS
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-        
+
+
 if __name__ == "__main__":
     main()
